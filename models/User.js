6be69443c48c5d628ba8bb31eeb723764bd08e32
Damirbek05 <124022133+Damirbek05@users.noreply.github.com>
2024-01-29 16:22:42 +0600

    Add files via upload
    folder models : User.js

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    avatarUrl: String,
+}, {
+    timstams: true,
+})
+
+export default mongoose.model('User',UserSchema);
