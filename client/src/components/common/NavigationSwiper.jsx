5c6039d602233b0af897c2bdf255cfd070565935
Damir Ospanbek <124022133+Damirbek05@users.noreply.github.com>
2024-02-22 21:13:17 +0600

    client

---
+++ client/src/components/common/NavigationSwiper.jsx
@@ -0,0 +1,39 @@
+import { Box } from "@mui/material";
+import { Swiper, Navigation, Pagination } from "swiper/react";
+
+const NavigationSwiper = ({ children }) => {
+  return (
+    <Box sx={{
+      "& .swiper-slide": {
+        width: "100%",
+        opacity: "0.6",
+        paddingBottom: "3rem"
+      },
+      "& .swiper-slide-active": { opacity: 1 },
+      "& .swiper-pagination-bullet": {
+        backgroundColor: "text.primary"
+      },
+      "& .swiper-button-next, & .swiper-button-prev": {
+        color: "text.primary",
+        "&::after": {
+          fontSize: { xs: "1rem", md: "2rem" }
+        }
+      },
+      "& .swiper": {
+        paddingX: { xs: "1rem", md: "4rem" }
+      }
+    }}>
+      <Swiper
+        spaceBetween={10}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+        navigation={true}
+        style={{ width: "100%", height: "max-content" }}
+      >
+        {children}
+      </Swiper>
+    </Box>
+  );
+};
+
+export default NavigationSwiper;
